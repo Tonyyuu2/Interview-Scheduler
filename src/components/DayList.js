9a2a98644e4be47eac780d8f5dc309f6d78f62ae
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-04-19 20:56:38 -0700

    add DayList to application.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,15 +5,13 @@ function DayList(props) {
   
   const dayList = props.days.map((day) => {
     return (
-      <ul key={day.id}>
-        <DayListItem
-        key={day.id}
-        name={day.name}
-        spots={day.spots}
-        selected={day.name === props.name}
-        setDay={props.setDay}
-        />
-      </ul>
+      <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.name}
+      setDay={props.setDay}
+      />
     )
   })
   
