967c98219fd1e9a566caa2c0bf294ca58a600762
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-04-20 20:47:41 -0700

    q

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,35 +2,42 @@ import React, { useState } from 'react'
 import InterviewerList from 'components/InterviewerList'
 import Button from 'components/Button'
 
-export default function Form() {
+export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || ""); //short
   const [interviewer, setInterviewer] = useState(props.interviewer || null); //short
+  
+  const reset = () => {
+    setStudent("")
+    setInterviewer(null)
+  }
 
+  const cancel = () => {
+    reset()
+    props.cancel()
+
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
-    <form autoComplete="off">
+    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
       <input
         className="appointment__create-input text--semi-bold"
         name="name"
         type="text"
+        value={student}
         placeholder="Enter Student Name"
-        /*
-          This must be a controlled component
-          your code goes here
-        */
+        onChange={(event) => setStudent(event.target.value)}
+        
       />
     </form>
-    <InterviewerList 
-      /* your code goes here */
-    />
+    <InterviewerList value={interviewer} interviewers={props.interviewers} onChange={setInterviewer} /> 
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
-      <Button danger {/* your code goes here */}>Cancel</Button>
-      <Button confirm {/* your code goes here */}>Save</Button>
+      <Button danger onClick={cancel}>Cancel</Button>
+      <Button confirm onClick={props.onSave}>Save</Button>
     </section>
   </section>
 </main>
