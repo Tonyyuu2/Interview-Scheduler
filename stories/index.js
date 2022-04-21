9f24d8106385ef258509f70e13f44d807a57c30b
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-04-20 17:41:29 -0700

    add Form component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,10 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm"
+import Status from "components/Appointment/Status"
+import Error from "components/Appointment/Error"
+import Form from "components/Appointment/Form"
 
 storiesOf("Button", module)
   .addParameters({
@@ -155,3 +159,8 @@ storiesOf("InterviewerList", module)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+  .add("Confirm", () => <Confirm onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+  .add("Status", () => <Status />)
+  .add("Error", () => <Error onClose={action("onClose")} />)
+  .add("Edit", () => <Appointment student="Tony" interviewer={1} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+  .add("Create", () => <Appointment interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
