50d19c2547877d79651420f97cdc9bb505c50b25
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-04-27 10:03:29 -0700

    add prettier to some files

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,4 +56,3 @@ export default function Application(props) {
     </main>
   );
 }
-
--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,24 +1,21 @@
-import React, { useState } from 'react'
-import InterviewerList from 'components/InterviewerList'
-import Button from 'components/Button'
+import React, { useState } from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
 
 export default function Form(props) {
-
-
   const [student, setStudent] = useState(props.student || ""); //short
   const [interviewer, setInterviewer] = useState(props.interviewer || null); //short
-  const [error, setError] = useState("")
-  
+  const [error, setError] = useState("");
+
   const reset = () => {
-    setStudent("")
-    setInterviewer(null)
-  }
+    setStudent("");
+    setInterviewer(null);
+  };
 
   const cancel = () => {
-    reset()
-    props.onCancel()
-
-  }
+    reset();
+    props.onCancel();
+  };
 
   function validate() {
     if (student === "") {
@@ -35,28 +32,35 @@ export default function Form(props) {
 
   return (
     <main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        value={student}
-        placeholder="Enter Student Name"
-        onChange={(event) => setStudent(event.target.value)}
-        data-testid="student-name-input"
-        
-      />
-    </form>
-    <section className="appointment__validation">{error}</section>
-    <InterviewerList value={interviewer} interviewers={props.interviewers} onChange={setInterviewer} /> 
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm onClick={() => validate()}>Save</Button>
-    </section>
-  </section>
-</main>
-  )
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            value={student}
+            placeholder="Enter Student Name"
+            onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
+          />
+        </form>
+        <section className="appointment__validation">{error}</section>
+        <InterviewerList
+          value={interviewer}
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={() => validate()}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
 }
