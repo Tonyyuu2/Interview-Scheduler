9bdeab63d387d1cb25851eebbaa1e0239cec67f3
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-04-27 18:05:33 -0700

    add cypress test and finish cypress

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -25,4 +25,40 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview", () => {
+    cy.get(
+      "#root > main > section.schedule > article:nth-child(1) > main > section.appointment__card-right > section > img:nth-child(1)"
+    )
+      .first()
+      .click({ force: true });
+
+    cy.get('[data-testid="student-name-input"]')
+      .clear()
+      .type("Tony Yu")
+      .get(":nth-child(2) > .interviewers__item-image")
+      .click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Tony Yu");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get(
+      "#root > main > section.schedule > article:nth-child(1) > main > section.appointment__card-right > section > img:nth-child(2)"
+    )
+      .first()
+      .click({ force: true });
+
+    cy.get(
+      "#root > main > section.schedule > article:nth-child(1) > main > section > button:nth-child(2)"
+    ).click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen").should(
+      "not.exist"
+    );
+  });
 });
